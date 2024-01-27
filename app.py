bd6fed196e6025d7b41fb439710663265c35de1c
unknown <bekhruzrajabov77@gmail.com>
2024-01-27 17:52:17 +0500

    First Commit

---
+++ app.py
@@ -0,0 +1,35 @@
+# a = int(input('Введите число: '))
+# b = int(input('Введите число: '))
+
+# print(a + b)
+
+# a = int(input('Введите число: '))
+# b = int(input('Введите число: '))
+# print((a + b)* 2)
+
+# a = int(input('Введите число: '))
+# b = int(input('Введите число: '))
+# print(a * b)
+
+# r = int(input('Введите число: '))
+# p = 3.14
+# c = 0
+# print(p * r **2)
+
+# a = float(input("Введите число: "))
+# b = float(input("Введите число: "))
+# c = float(input("Введите число: "))
+# print(a + b + c)
+
+# k = int(input('Введите число: '))
+# n = int(input('Введите число: '))
+# print(k / n) 
+# print(k % n)
+# Garou = 4
+
+# a = 667 - 13
+# b = 101 + a
+# c = a + b
+# print(a, b, c)
+# print(56)
+# exit()
